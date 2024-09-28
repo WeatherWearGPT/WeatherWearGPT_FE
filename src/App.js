5c3913bf631e33ddf70b0be41e7d45970e356a8a
jiyeon <thvldk1654@naver.com>
2024-09-29 03:20:25 +0900

    +mypage

--- src/App.js
+++ src/App.js
@@ -3,25 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Loginform from './components/Loginform'; // 로그인 페이지
 import Sign from './components/Sign'; // 일반 회원가입 페이지
 import SignSocial from './components/SignSocial'; // 소셜 로그인 사용자 추가 정보 페이지
-
+import Mypage from './components/mypage';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <Routes>
-            {/* 로그인 페이지 */}
-            <Route path="/" element={<Loginform />} />
+      <Routes>
+        {/* 기본 경로 (/)로 접속하면 마이페이지를 보여줍니다 */}
+        <Route path="/" element={<Mypage />} />
 
-            {/* 일반 회원가입 페이지 */}
-            <Route path="/signup" element={<Sign />} />
-
-            {/* 소셜 로그인 사용자 추가 정보 입력 페이지 */}
-            <Route path="/social-signup" element={<SignSocial />} />
-          </Routes>
-        </header>
-      </div>
+        {/* 로그인 페이지 경로 */}
+        <Route path="/loginform" element={<Loginform />} />
+      </Routes>
     </Router>
   );
 }
