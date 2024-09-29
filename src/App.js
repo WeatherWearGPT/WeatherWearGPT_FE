1e6708612284ecb575f60dc2593f92a0b683957a
jiyeon <thvldk1654@naver.com>
2024-09-30 04:11:10 +0900

    chat

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+
 import Loginform from './components/Loginform'; // 로그인 페이지
 import Sign from './components/Sign'; // 일반 회원가입 페이지
 import SignSocial from './components/SignSocial'; // 소셜 로그인 사용자 추가 정보 페이지
 import Mypage from './components/mypage';
+import Chat from './components/chat';  // Chat.js 컴포넌트 임포트
+import './App.css';        // App.css 스타일 임포트
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* 기본 경로 (/)로 접속하면 마이페이지를 보여줍니다 */}
-        <Route path="/" element={<Mypage />} />
+    <div className="page-container">
+      {/* 1번 구역 */}
+      <div className="section-one">
+        <h2>1번 화면 - 1/3 크기</h2>
+      </div>
 
-        {/* 로그인 페이지 경로 */}
-        <Route path="/loginform" element={<Loginform />} />
-      </Routes>
-    </Router>
+      {/* 2번 구역 (chat.js가 동작하는 곳) */}
+      <div className="section-two">
+        <Chat />
+      </div>
+    </div>
   );
 }
 
 export default App;
+
