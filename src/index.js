f2b65e2c60d67be3b5aa0e9c99e55f1b13edbd33
jiyeon <thvldk1654@naver.com>
2024-10-02 05:59:27 +0900

    chat api connect try

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+//const API_BASE_URL = process.env.REACT_APP_API_BASE_URL; //절대경로 url
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
